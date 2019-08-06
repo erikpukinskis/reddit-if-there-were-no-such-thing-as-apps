be08e7a2bb7bb14ed0d1aebb9cf8ceda48d5ba5c
Erik Pukinskis <erik@snowedin.net>
2019-08-06 00:12:48 -0700

    Top bar

--- reddit.js
+++ reddit.js
@@ -1,50 +1,39 @@
 var library = require("module-library")(require)
 
+// The Reddit Everyone Deserves Dang It
+
 library.using(
-  [library.ref(), "web-site", "browser-bridge", "web-element", "add-html"],
-  function(lib, WebSite, BrowserBridge, element, addHtml) {
+  ["web-site", "browser-bridge", "web-element", "add-html", "crypto"],
+  function(WebSite, BrowserBridge, element, addHtml, crypto) {
 
     var bridge = new BrowserBridge()
     var site = new WebSite()
 
+    rando(function(rand) {
+      var redditOauthUrl = "https://www.reddit.com/api/v1/authorize?client_id="+process.env.REDDIT_CLIENT_ID+"&response_type=code&state="+rand+"&redirect_uri="+encodeURIComponent("http://www.redditiftherewerenosuchthingasapps.com")+"&duration=permanent&scope=mysubreddits"
+      console.log(redditOauthUrl)
+    })
+
+
+    function rando(callback) {
+      crypto.randomBytes(
+        32,
+        function(error, buffer) {
+          if (error) throw error
+          callback(buffer.toString(
+            'hex'))})}
+
     site.addRoute(
       "get",
-      "/reddit.gif",
+      "/treddi.gif",
       site.sendFile(
         __dirname,
-        "reddit.gif"))
+        "treddi.gif"))
 
-    var icon = element.template(
-      ".icon",
-      element.style({
-        "background": "url(/reddit.gif)",
-        "overflow": "hidden",
-        "width": "44px",
-        "height": "44px",
-
-        ".icon-large": {
-          "width": "88px",
-          "height": "88px"},
-
-        ".icon-wide": {
-          "width": "152px",
-          "height": "66px"},
-
-        ".icon-medium": {
-          "width": "66px",
-          "height": "66px"},
-      }),
-      function(size, position) {
-        this.appendStyles({
-          "background-position": "-"+position[0]+"px -"+position[1]+"px"
-        })
-        if (size) {
-          this.addSelector(
-            ".icon-"+size)}})
 
     var positionOfIcon = {
       "alien": [14,14],
-      "logotype": [107,20],
+      "logotype": [105,25],
       "choice": [260, 23],
       "pencil": [677,29],
       "search": [755,32],
@@ -67,16 +56,58 @@ library.using(
       "hamburger": "medium",
     }
 
-    var icons = []
-    for(var name in positionOfIcon) {
-      icons.push(
-        icon(
-          sizeOfIcon[
-            name],
-          positionOfIcon[
-            name]))
-      icons.push(
-        name)}
+    var icon = element.template(
+      ".icon",
+      element.style({
+        "background": "url(/treddi.gif)",
+        "overflow": "hidden",
+        "display": "inline-block",
+        "vertical-align": "middle",
+        "width": "44px",
+        "height": "44px",
+        "box-sizing": "border-box",
+
+        ".icon-large": {
+          "width": "88px",
+          "height": "88px"},
+
+        ".icon-wide": {
+          "width": "152px",
+          "height": "66px"},
+
+        ".icon-medium": {
+          "width": "66px",
+          "height": "66px"},
+      }),
+      function(name) {
+        var size = sizeOfIcon[
+          name]
+        var position = positionOfIcon[
+          name]
+        this.appendStyles({
+          "background-position": "-"+position[0]+"px -"+position[1]+"px"})
+        if (size) {
+          this.addSelector(
+            ".icon-"+size)}})
+
+
+
+    var page = element(
+      element.style({
+        "width": "100%",
+        "display": "flex",
+        "flex-direction": "row",
+        "justify-content": "space-between"}),[
+      element(
+        icon("alien"),
+        icon("logotype"),
+        icon("choice")),
+      element(
+        icon("pencil"),
+        icon("search")),
+    ])
+
+
 
     var stylesheet = element.stylesheet([
       icon,
@@ -89,8 +120,6 @@ library.using(
     bridge.addToHead(
       stylesheet)
 
-    var page = element(
-      icons)
     
     site.addRoute("get", "/", bridge.requestHandler(page))
 
