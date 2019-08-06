b107f9459e521a50a3eeb30d2fc6256dff895494
Erik Pukinskis <erik@snowedin.net>
2019-08-06 01:06:01 -0700

    Everything we need for the front page

--- reddit.js
+++ reddit.js
@@ -40,11 +40,12 @@ library.using(
       "hamburger": [840,29],
       "rocket": [23,160],
       "lil-choice": [168,160],
-      "escape": [152,358],
+      "escape": [152,360],
       "share": [36,416],
       "bubble": [243,420],
+      "dotdotdot": [584,426],
       "updoot": [686,426],
-      "downdoot": [832,426],
+      "downdoot": [832,428],
     }
 
     var sizeOfIcon = {
@@ -84,6 +85,9 @@ library.using(
           name]
         var position = positionOfIcon[
           name]
+        if (!position) {
+          throw new Error(name+" is not an icon")
+        }
         this.appendStyles({
           "background-position": "-"+position[0]+"px -"+position[1]+"px"})
         if (size) {
@@ -92,7 +96,7 @@ library.using(
 
 
 
-    var page = element(
+    var header = element(
       element.style({
         "width": "100%",
         "display": "flex",
@@ -104,9 +108,105 @@ library.using(
         icon("choice")),
       element(
         icon("pencil"),
-        icon("search")),
+        icon("search"),
+        icon("hamburger")),
     ])
 
+    var sort = element(
+      element.style({
+        "width": "100%",
+        "display": "flex",
+        "flex-direction": "row",
+        "margin": "44px 0"}),[
+        icon("rocket"),
+        element(
+          element.style({
+            "margin": "0 0.5em"}),
+          "Best"),
+        icon("lil-choice")])
+
+    var dot = element.template(
+      ".dot",
+      element.style({
+        "margin": "0 8px"}),
+      "&bull;")
+
+    var meta = element(
+      element.style({
+        "display": "flex",
+        "flex-direction": "row"}),[
+      element(".sub", "r/funny"),
+      dot(),
+      "5h",
+      dot(),
+      "u/charbodactyl",
+      dot()])
+
+    var domain = element(
+      "a",{
+      "href": ""},
+      element.raw("i.redd.it "),
+      icon("escape"))
+
+    var actions = element(
+      ".actions",
+      element.style({
+        "width": "100%",
+        "line-height": "66px",
+        "display": "flex",
+        "flex-direction": "row",
+        "justify-content": "space-between"}),[
+      element(
+        element.style({
+          "display": "flex",
+          "flex-direction": "row"}),[
+        element(
+          element.style({
+            "cursor": "pointer",
+            "padding-right": "44px"}),
+          icon("share"),
+          " Share"),
+        element(
+          icon("bubble"),
+          " 257",
+          element.style({
+            "cursor": "pointer",
+            "padding-right": "44px"})),
+      ]),
+      element(
+        element.style({
+          "display": "flex",
+          "flex-direction": "row"}),[
+        element(
+          element.style({
+            "cursor": "pointer",
+            "border-right":
+              "2px solid #cfcfff",
+            "padding-right": "22px"}),
+          icon("dotdotdot")),
+        element(
+          element.style({
+            "cursor": "pointer",
+            "padding-left": "22px"}),
+          icon("updoot"),
+          "27.2k"),
+        element(
+          element.style({
+            "padding-left": "22px"}),
+          icon("downdoot")),
+      ]),
+    ])
+
+    var post = element([
+      meta,
+      element(
+        ".title",
+        "Didn't like his new friend"),
+      domain,
+      actions,
+    ])
+
+    var page = [header, sort, post]
 
 
     var stylesheet = element.stylesheet([
@@ -114,7 +214,26 @@ library.using(
 
       element.style(
         "body", {
+          "margin": "15px",
+          "font-size": "32px",
+          "line-height": "44px",         
+          "color": "#cfcfff",
           "font-family": "sans-serif"}),
+
+      dot,
+
+      element.style(
+        ".sub",{
+        "color": "aquamarine"}),
+
+      element.style(
+        ".title",{
+        "color": "black"}),
+
+      element.style(
+        "a", {
+        "color": "#5EE8EE",
+        "text-decoration": "none"})
     ])
 
     bridge.addToHead(
